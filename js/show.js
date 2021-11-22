57324c3b7e90b56f131484afc215033701030ce7
UltraRaptorYT <62703912+UltraRaptorYT@users.noreply.github.com>
2021-11-22 13:58:01 +0800

    Update show.js

--- js/show.js
+++ js/show.js
@@ -27,7 +27,7 @@ var movingText = () => {
     mm = String(today.getMonth() + 1).padStart(2, "0"),
     yyyy = today.getFullYear(),
     nextYear = yyyy + 1,
-    dayMonth = "11/21/",
+    dayMonth = "11/22/",
     birthday = dayMonth + yyyy;
 
   today = mm + "/" + dd + "/" + yyyy;
